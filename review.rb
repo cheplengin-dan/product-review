951a2f99f9f5bf03d55c401a94be2222938e6629
duncan kiptoo <cheplenginduncan@gmail.com>
2023-06-09 17:03:13 +0300

    add Product

--- review.rb
+++ review.rb
@@ -32,3 +32,49 @@ class User
     end
   end
   
+  class Product
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+      @reviews = []
+      @users = []
+    end
+  
+    def add_review(review)
+      @reviews << review
+      review.product = self
+    end
+  
+    def add_user(user)
+      @users << user
+      user.add_product(self)
+    end
+  
+    def reviews
+      @reviews.dup
+    end
+  
+    def users
+      @users.dup
+    end
+  
+    def leave_review(user, star_rating, comment)
+      review = Review.new(user, self, star_rating, comment)
+      @reviews << review
+      user.add_review(review)
+    end
+  
+    def print_all_reviews
+      @reviews.each do |review|
+        puts "Review for #{name} by #{review.user.name}: #{review.star_rating}. #{review.comment}"
+      end
+    end
+  
+    def average_rating
+      total_ratings = @reviews.map(&:star_rating).reduce(0, :+)
+      total_reviews = @reviews.size
+  
+      total_reviews > 0 ? (total_ratings.to_f / total_reviews) : 0
+    end
+  end
